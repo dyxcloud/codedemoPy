c39586d5bcd39a2753c49dd4e6d86b24e46e5f08
dyxLike <dx01@live.cn>
2019-06-10 23:24:18 +0800

    renanme file2

--- tools/Rename.py
+++ tools/Rename.py
@@ -4,9 +4,9 @@ path="D:\OneDrive - business\Video\A+\DARLING in the FRANXX\Scans"
 
 for root,dirs,files in os.walk(path):
     for dir in dirs:
-        dir1 = root+"/"+dir
+        dir1 = root+"/"+dir+"/" #vol文件夹
         for root1,dirs1,files1 in os.walk(dir1):
             for file in files1:
                 if(str(file).endswith(".jpg")):
-                    print(dir1+"/"+file+" to "+dir1+"/"+dir+".jpg")
-                    os.rename(dir1+"/"+file,dir1+"/"+dir+".jpg")
+                    print(dir1+file+" to "+dir1+dir+".jpg")
+                    os.rename(dir1+file,dir1+dir+".jpg")
