7abaf90692dbb7baa0bbb49a9e4c328fcd365051
dyxLike <dx01@live.cn>
2019-06-10 23:20:58 +0800

    rename file

---
+++ tools/Rename.py
@@ -0,0 +1,12 @@
+import os
+
+path="D:\OneDrive - business\Video\A+\DARLING in the FRANXX\Scans"
+
+for root,dirs,files in os.walk(path):
+    for dir in dirs:
+        dir1 = root+"/"+dir
+        for root1,dirs1,files1 in os.walk(dir1):
+            for file in files1:
+                if(str(file).endswith(".jpg")):
+                    print(dir1+"/"+file+" to "+dir1+"/"+dir+".jpg")
+                    os.rename(dir1+"/"+file,dir1+"/"+dir+".jpg")
