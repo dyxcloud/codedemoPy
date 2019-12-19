24876e0272d516763c65bd08b13e7cc70b81bfc8
dyxLike <dx01@live.cn>
2019-12-19 16:49:26 +0800

    pixiv fix reg

--- ApiTest.py
+++ ApiTest.py
@@ -1,15 +1,5 @@
-from urllib import request
-import sys
+import re
 
-
-url = "https://cdn.v2ex.com/avatar/4bd1/ee3f/275922_large.png123"
-req = request.Request(url)
-try:
-    with request.urlopen(req,timeout=15) as f:
-            if f.status == 200:
-                data = f.read()
-                print(len(data))
-            else:
-                print(f.status,f.reason)
-except :
-    print("Unexpected error:", sys.exc_info()[:2])
+str1 = "123@qq.comaaa@163.combbb@126.comasdf111@asdfcom"
+print(re.findall(r'\w+@(?:qq|163|126)\.com',str1))
+print(re.match(r'\w+@(?:qq|163|126)\.com',str1).groups())
