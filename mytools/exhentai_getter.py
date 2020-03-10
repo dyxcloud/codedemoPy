20aa8c6eac28d3faedc7ab3a55fa4c60c26cffa8
dyxLike <dx01@live.cn>
2020-03-10 17:10:32 +0800

    nhentai add

--- mytools/exhentai_getter.py
+++ mytools/exhentai_getter.py
@@ -1,6 +1,6 @@
 import requests_html
 
-class hentaigetter:
+class exhentaigetter:
 
     heads = {}
     session = requests_html.HTMLSession()
@@ -29,12 +29,50 @@ class hentaigetter:
         for src in srcs:
             print(src)
 
+class nhentaigetter:
+
+    heads = {}
+    proxy = {'https':'127.0.0.1:25378'}
+    session = requests_html.HTMLSession()
+
+    def __init__(self,cookie):
+        user_agent = requests_html.user_agent()    
+        self.heads['User-Agent'] = user_agent
+        self.heads['Cookie'] = cookie
+
+    def _get_smallimg_url(self,url):
+        r = self.session.get(url, headers=self.heads,proxies=self.proxy)
+        es = r.html.find('.gallerythumb > img')
+        return [e.attrs['data-src'] for e in es]
+    
+    def trans_smallimg(self,url):
+        '''https://t.nhentai.net/galleries/1502218/1t.jpg
+        to https://i.nhentai.net/galleries/1502218/1.jpg
+        '''
+        url = url.replace('https://t.nhentai.net','https://i.nhentai.net',1)
+        url = url.replace('t.jpg','.jpg',1)
+        return url
+
+    def getAllImg(self,url):
+        urls = self._get_smallimg_url(url)
+        return [self.trans_smallimg(u) for u in urls]
+
+    def print_result(self,url):
+        srcs = self.getAllImg(url)
+        for src in srcs:
+            print(src)
+
+
+
+
 #TODO 漫画预览页超过一页 添加翻页功能
 
 if __name__ == "__main__":
-    cookie = 'sl=dm_1; ipb_member_id=4483572; ipb_pass_hash=b1d7d5acd649a01a1643124c8a0918a8; igneous=df9724040; sk=; yay=0; panda_width=1500'
-    getter = hentaigetter(cookie)
+    # cookie = 'sl=dm_1; ipb_member_id=4483572; ipb_pass_hash=b1d7d5acd649a01a1643124c8a0918a8; igneous=df9724040; sk=; yay=0; panda_width=1500'
+    # getter = exhentaigetter(cookie)
+    # url = 'https://exhentai.org/g/1362635/816a598ece/'
+    # getter.print_result(url)
 
-    url = 'https://exhentai.org/g/688441/09e456b8f5/'
-    getter.print_result(url)
-    print("done")
+    ngetter = nhentaigetter('')
+    url = 'https://nhentai.net/g/221928/'
+    ngetter.print_result(url)
