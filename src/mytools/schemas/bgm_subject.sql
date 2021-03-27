36378e9b94c55cb187fb4da0770602b43156d2b1
dyxLike <dx01@live.cn>
2021-03-27 18:56:47 +0800

    sql结构

---
+++ src/mytools/schemas/bgm_subject.sql
@@ -0,0 +1,18 @@
+create table bgm_subject
+(
+    subject_id integer not null
+        constraint bgm_subject_pk
+            primary key,
+    name varchar not null,
+    name_cn varchar,
+    point double precision default 0.0,
+    rank integer default 0,
+    votes integer default 0,
+    date varchar,
+    wanted integer default 0,
+    watched integer default 0,
+    watching integer default 0,
+    hold integer default 0,
+    "drop" integer default 0,
+    create_date varchar
+);
